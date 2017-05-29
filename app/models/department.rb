c5248b3a7590c978019b2bd828242d6672a36007
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-05-29 17:39:24 +0900

    departmentにstateを追加
    stateをenumで定義

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,4 +2,6 @@ class Department < ApplicationRecord
   has_one :user
 
   validates :department_name, presence: true
+
+  enum status: { invalidate: 0, activate: 1 }
 end
---
+++ db/migrate/20170529082141_add_state_to_departments.rb
@@ -0,0 +1,5 @@
+class AddStateToDepartments < ActiveRecord::Migration[5.1]
+  def change
+    add_column :departments, :state, :integer
+  end
+end
