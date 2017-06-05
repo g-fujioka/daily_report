4d44f4a8a1ba3cafb688df27dfe51c8c3f54c19c
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-06-05 11:50:42 +0900

    ログアウトボタンを押したときログイン画面にリダイレクトするテストを追加

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions', type: :request do
+  let(:user) { FactoryGirl.create(:user) }
   describe 'GET /login' do
-    let(:user) { FactoryGirl.create(:user) }
     it 'ログイン画面が表示されること' do
       get login_path
       expect(response).to have_http_status(200)
@@ -27,6 +27,7 @@ RSpec.describe 'Sessions', type: :request do
     end
 
     context 'パスワードが空白の場合' do
+
       it 'ログインに失敗すること' do
         post login_path, params: { session: { email: user.email, password: '' } }
         expect(response).to_not have_http_status(302)
@@ -34,6 +35,16 @@ RSpec.describe 'Sessions', type: :request do
       it 'flash[:danger]にメッセージが含まれること' do
         post login_path, params: { session: { email: user.email, password: '' } }
         expect(flash[:danger]).to include 'メールアドレス、または、パスワードが違います'
+      end
+    end
+  end
+
+  describe 'Delete /logout' do
+    context 'ログアウトボタンを押したとき' do
+      it 'ログイン画面にリダイレクトすること' do
+        post login_path, params: { session: { email: user.email, password: user.password } }
+        delete logout_path
+        expect(response).to redirect_to login_url
       end
     end
   end
