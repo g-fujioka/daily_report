5ff791794da25829d41bdc8a25fcd5aba6f09528
Gakuto Fujioka <g-fujioka@rhizome-e.com>
2017-06-08 10:55:55 +0900

    capistranoの設定
    developのhostを""に指定
    
    directory名をdeployからunicornに変更
    
    Revert "directory名をdeployからunicornに変更"
    
    This reverts commit 6a707a1bea89efba1e89a2acf60ce85b41d91dbd.
    
    pathの修正
    
    secrets.ymlのコピー
    
    taskの編集
    
    capistranoの編集ファイルを変更
    
    不要なファイルの削除、branchの変更

---
+++ config/deploy/production.rb
@@ -0,0 +1,21 @@
+server '192.168.56.100', user: 'g-fujioka', roles: %w{app db web}
+set :unicorn_pid, "/home/g-fujioka/#{fetch(:application)}/shared/tmp/pids/unicorn.pid"
+set :unicorn_rack_env, 'production'
+set :unicorn_config_path, "/home/g-fujioka/#{fetch(:application)}/current/config/unicorn.rb"
+set :rails_env, 'production'
+set :branch, 'master'
+set :migration_role, 'db'
+set :ssh_options, {
+    keys: [File.expand_path('~/.ssh/id_rsa.pem)')]
+}
+
+namespace :deploy do
+  desc 'secrets.ymlをコピーします'
+  task :copy_secrets_yml do
+    on roles(:all) do
+      execute "cp #{shared_path}/secrets.yml #{release_path}/config/secrets.yml"
+    end
+  end
+end
+
+before 'unicorn:start', 'deploy:copy_secrets_yml'
